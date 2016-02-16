3855c6fe6e70a345847a0e2b1caae9d752cd4f35
asayles <asayles@pdx.edu>
2016-02-15 16:59:47 -0800

    data processing finishing

--- data_processing.py
+++ data_processing.py
@@ -1,110 +1,66 @@
 #!/usr/bin/python
 
-import csv
+import pandas
 import numpy as np
+from sklearn import preprocessing as pp
 
+"""
+Read file 
+Split into pos,neg classified sets
+Truncate the larger set
+Join half the pos and neg data as training, test
+Shuffle both data sets to eliminate ordering
+Scale the data
+    Create float matrix
+    Create a scaled copy of matrix
+    Create array of classifications
+    Repeat for test
+Return train_feat_scaled, train_class, test_feat_scaled, test_class
+"""
+def magic_data_scaler():
+    data_raw = pandas.read_csv('/home/ajsayles/School/cs445/data_raw.csv', header=None)
 
-def load_spam_data(filename = 'data_raw'):
-    
-    file_data_raw = file(filename,'rb')
-    data_raw = file_data_raw.readlines()
-    
-    labels = []
-    features = []
-    
-    for line in data_raw:
-        feature_vector = []
-        split_line = line.split(',')
-        
-        for element in split_line[:-1]:
-            feature_vector.append(float(element))
-            
-        features.append(feature_vector)
-        labels.append(int(split_line[-1]))
-        
-    return features, labels
-    
-def balance_dataset(features, labels):
+    class_pos = (data_raw.loc[data_raw[57] == 1])
+    # print class_pos.shape[0]
+    pos = class_pos.shape[0]
+    class_neg = (data_raw.loc[data_raw[57] == 0])
+    # print class_neg.shape[0]
+    neg = class_neg.shape[0]
 
-    count_0 = labels.count(0)
-    count_1 = labels.count(1)
-    balanced_count = min(count_0, count_1)
-    
-    return features[:balanced_count] + features[-balanced_count], labels[:balanced_count] + labels[-balanced_count:]
-    
-def ConvertDataToArrays(features, labels):
-  """
-  conversion to a numpy array is easy if you're starting with a List of lists.
-  The returned array has dimensions (M,N), where M is the number of lists and N is the number of 
-  
-  """
-
-  return np.asarray(features), np.asarray(labels)
-  
-
-def NormalizeFeatures(features):
-  """
-  I'm providing this mostly as a way to demonstrate array operations using Numpy.  Incidentally it also solves a small step in the homework.
-  """
-  
-  "selecting axis=0 causes the mean to be computed across each feature, for all the samples"
-  means = np.mean(features, axis = 0)
-  
-  variances = np.var(features, axis = 0)
-
-  "Operations in numpy performed on a 2D array and a 1D matrix will automatically broadcast correctly, if the leading dimensions match."
-  features = features - means
-  # features -= means
-
-  features /= variances
-
-  return features
-
-
-def PrintDataToSvmLightFormat(features, labels, filename = "svm_features.data"):
-  """  
-  Readable format for SVM Light should be, with 
-  lable 0:feature0, 1:feature1, 2:feature2, etc...
-  where label is -1 or 1.
-  """
-
-  if len(features) != len(labels):
-    raise Exception("Number of samples and labels must match")
-  dat_file = file(filename,'w')
-  for s in range(len(features)):
-
-    if labels[s]==0:
-      line="-1 "
+    if pos > neg:
+        # impliment later if there is time
+        print "more positive classifications" 
     else:
-      line="1 "
+        # print "more neg examples"
+        class_neg = class_neg[0:(pos -1)].reset_index(drop=True)
+        
 
-    for f in range(len(features[s])):
-      line +="%i:%f " % (f+1 , features[s][f])
-    line += "\n"
-    dat_file.write(line)
-  dat_file.close()
+    training_superset = [class_pos[0:(pos / 2) - 1], class_neg[0:(pos / 2) - 1]]
+    training_frame = pandas.concat(training_superset)
+    training_frame = training_frame.sample(frac=1).reset_index(drop=True)
+    # print training_frame
 
+    test_superset = [class_pos[(pos /2):pos - 2], class_neg[(pos / 2):pos - 2]]
+    test_frame = pandas.concat(test_superset)
+    test_frame = test_frame.sample(frac=1).reset_index(drop=True)
+    # print test_frame
 
-def FeatureSubset(features_array, indices):
-  """
-  Takes the original set of features and returns a small array containing only the features with the given indices.
+    training_frame = training_frame.sample(frac=1).reset_index(drop=True)
+    test_frame = test_frame.sample(frac=1).reset_index(drop=True)
+    # print training_frame
 
-  features_array is a numpy 2D array of dimension (M,N), where M is the number of samples and N is the number of features in the feature vector.
+    X_train = training_frame.as_matrix().astype(np.float)
+    # print X
+    X_train_feat_scaled = pp.StandardScaler().fit_transform(X_train[:,:57])
+    # print X_train_feat_scaled
+    X_train_class = X_train[:,57]
+    # print X_train_class
 
-  indices are those of the features to be used, as a list of integers  
-  """
-  return features_array[:,indices]
-
-def main():
-
-  features, labels = LoadSpamData()
-  features, labels = BalanceDataset(features, labels)
-  features, labels = ConvertDataToArrays(features, labels)
-  features = NormalizeFeatures(features)
-  indices = [0,1,2]
-  features = FeatureSubset(features, indices)
-
-  PrintDataToSvmLightFormat(features, labels)
-
-if __name__ == "__main__":
-  main()
+    X_test = test_frame.as_matrix().astype(np.float)
+    # print X_test
+    X_test_feat_scaled = pp.StandardScaler().fit_transform(X_test[:,:57])
+    # print X_test_feat_scaled
+    X_test_class = X_test[:,57]
+    # print X_test_class
+    
+    return X_train_feat_scaled,X_train_class,X_test_feat_scaled,X_test_class
